3a1c6b31b00048d7b1db9d2d9438f8a09bb33096
Mose Richardson <57810061+MSPFE2019@users.noreply.github.com>
2021-05-14 18:33:34 -0500

    Update RemoveEveryone.ps1

--- RemoveEveryone.ps1
+++ RemoveEveryone.ps1
@@ -1,28 +1,12 @@
-#region Parameters
-#url https://yourdomain-admin.sharepoint.com
-$URL="https://gov210-admin.sharepoint.com"  
-#endregion Parameters
 
-# region Get Site Collecions   
-Connect-PnPOnline -Url $URL -cred o365
-## Powershell will prompt for sign
-## Copy Url from msg and code
-## Consent form only click ok, don't consent for entire org
-
-
-# Get the site collections  
-$siteColl=Get-PnPTenantSite  
-#endregion Get Site Collecions
-
-# Loop through the site collections  
-foreach($site in $siteColl)  
-{  
-  Connect-PnPOnline -Url $site.URL -cred o365
+Import-Csv .\Site.csv | ForEach-Object {
+  Write-Host "Working on $($_.Url)"
+  Connect-PnPOnline -Url $_.Url  -cred o365
 ##Remove Everyone Except External Users 
   Remove-PnPUser -Identity "Everyone except external users" -Confirm:$false
-  write-host -ForegroundColor Green "Removing Everyone except external users from: " $site.Url  
+  write-host -ForegroundColor Green "Removing Everyone except external users from: " $_.Url    
 
 ##Remove Everyone 
   Remove-PnPUser -Identity "Everyone" -Confirm:$false
-  write-host -ForegroundColor Green "Removing Everyone from: " $site.Url  
+  write-host -ForegroundColor Green "Removing Everyone from: " $_.Url  
   }
