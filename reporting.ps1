2d128d1b129af5613038f3064a2670a6fbfeaa27
Mose Richardson <57810061+MSPFE2019@users.noreply.github.com>
2021-06-17 15:20:13 -0500

    Update reporting.ps1

--- reporting.ps1
+++ reporting.ps1
@@ -1,24 +1,15 @@
 
 #### 
 ###Run this on the server that will remove the script
-####Add-PnPStoredCredential -Name "https://tenant.sharepoint.com" -Username yourname@tenant.onmicrosoft.com -Password (ConvertTo-SecureString -String "YourPassword" -AsPlainText -Force)
+####Add-PnPStoredCredential -Name "O365" -Username yourname@tenant.onmicrosoft.com -Password (ConvertTo-SecureString -String "YourPassword" -AsPlainText -Force)
 
-#region Parameters
-#url https://yourdomain-admin.sharepoint.com
-$URL='https://GOV210645-admin.sharepoint.com' 
-$group = 'JohannaL@GOV210645.OnMicrosoft.com'
 
+#### Add $owner to each site collection to get permission for the site collection and webs
+
+$Owner = "JohannaL@GOV0645.OnMicrosoft.com"
 #endregion Parameters
 
-#region Credentials
-
-# region Get Site Collecions   
-Connect-PnPOnline -Url $URL -cred o365
-## Powershell will prompt for sign
-## Copy Url from msg and code
-## Consent form only click ok, don't consent for entire org
-
-# Get the site collections  
+#Get all the site collections  
 $siteColl=Get-PnPTenantSite  
 #endregion Get Site Collecions
 
@@ -27,28 +18,24 @@ foreach($site in $siteColl)
 {  
   Connect-PnPOnline -Url $site.URL  -Credentials o365
   
-  Start-Sleep -Seconds 10
+### Add $Owner to the Site Collection - Needed to get all the permissions
+Set-PnPTenantSite -identity $site.URL -Owners $Owner
 
-Set-PnPTenantSite -identity $site.URL -Owners $group
+### Get Users Permission
+##### Replace Mose with how the user name is displayed in O365
+$perm = Get-PnPUser -withrightsAssignedDetailed | Where-Object {$_.Title.Startswith('Diego')}
 
-$FormatEnumerationLimit=-1
-$perm = Get-PnPUser -withrightsAssignedDetailed | where {$_.Title.Startswith('Diego')}
-$Perm = [pscustomobject]@{'Title' = $perm.Title; 'Groups' = $perm.Groups; 'Permissions' = $perm.Permissions}
-$perm | Select Title, @{Name ='Groups';expression={[string]::join(";",($_.Groups))}}, @{Name ='Permissions';expression={[string]::join(";",($_.Permissions))}}| Export-Csv -Path PermFile.csv -Append -force 
-$perm
-Get-PnPTenantSite -Identity $site.url 
-  write-host -ForegroundColor Green "Working on " $site.Url
-  Start-Sleep -Seconds 10
-#Get-PnPUser | ? Title -like $group | Remove-PnPSiteCollectionAdmin
+$perm = [pscustomobject]@{'Url'= $site.url;'User' = $perm.Title;'Groups' = $perm.Groups; 'Permissions' = $perm.Permissions; }
+
+###Export to CSV
+
+$perm | Select-Object  Url,User, @{Name ='Groups';expression={[string]::join(";",($_.Groups))}}, @{Name ='Permissions';expression={[string]::join(";",($_.Permissions))}} | Export-Csv -Path PermFile.csv -Append -force 
+
+write-host -ForegroundColor Green "Working on " $site.Url 
+ 
+  #### Remove $owner from the site
+  Get-PnPUser | Where-Object Email -eq $Owner | Remove-PnPUser -Confirm:$false
 
 
 }  
 
-
-Get-PnPTenantSite | ForEach-Object {
-   $site = $_
-   Write-Host "Processing $($site.Url)..."
-   Connect-PnPOnline $site.Url -Credentials o365
-   Get-PnPUser | ? Email -like $group | Remove-PnPSiteCollectionAdmin
-   Get-PnPSiteCollectionAdmin
-}
